d9c97cd542a3ad5533e6fe9fe4614fecaea2b515
NAYEEM65 <mathnayeem054@gmail.com>
2022-10-08 16:13:16 +0600

    private file ignored

--- src/api.js
+++ src/api.js
@@ -1,11 +1,11 @@
 export const geoApiOptions = {
     method: 'GET',
     headers: {
-        'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-        'X-RapidAPI-Host': process.env.REACT_APP_RAPID_API_HOST,
+        'X-RapidAPI-Key': '7af762c16fmsh146b66bb8a2cc92p196c36jsn63e8b392ad31',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com',
     },
 };
 
-export const GEO_API_URL = process.env.REACT_APP_GEO_API_URL;
-export const WEATHER_API_URL = process.env.REACT_APP_WEATHER_API_URL;
-export const WEATHER_API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+export const WEATHER_API_KEY = '6bf7b3e196cf81f3b6b74b30e45cd0ed';
